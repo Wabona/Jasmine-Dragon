4dbae56873c68ba00a195318d095e071536d879c
Chiku Okechukwu <chikuokechukwu@n3-6-80.dhcp.drexel.edu>
2023-05-24 14:45:03 -0400

    first react

---
+++ Components/navbar.jsx
@@ -0,0 +1,22 @@
+import "./navbarcss.css"
+
+export default function navbar() {
+return (
+<header>
+<nav className="nav">
+
+<div class="topnav">
+                
+               <div> <a class="active" href="#home">Home</a> </div>
+                <div class="therest">
+                <a class="tea" href="#news">Teas</a>
+                <a href="#contact">Poems</a>
+                </div>
+
+    </div>
+    
+</nav>
+</header>
+)
+}
+
---
+++ src/body.jsx
@@ -0,0 +1,20 @@
+import "./body.css"
+
+export default function body() {
+  return( 
+     <>
+        <div class="button">
+        <button>
+            
+        </button>
+      </div>
+     <div class="mainbody">
+          <img src="../src/images/lotus.jpg"></img>
+      </div>
+      
+   
+          </>
+  )
+
+}
+

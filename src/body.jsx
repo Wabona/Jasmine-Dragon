0bd70631ef9570a988c0e32abaeb3cd3e7be7434
Chiku Okechukwu <chikuokechukwu@Chikus-MacBook-Air.local>
2023-05-27 20:29:45 -0400

    Override! New and Improved

--- src/body.jsx
+++ src/body.jsx
@@ -1,15 +1,23 @@
 import "./body.css"
+import cupski from "./images/cup.png"
+import iroh from "./images/irohclip1.mp4"
+
 
 export default function body() {
   return( 
      <>
-        <div class="button">
-        <button>
-            
-        </button>
-      </div>
-     <div class="mainbody">
+    
+   {/*} <div class="mainbody">
           <img src="../src/images/lotus.jpg"></img>
+      </div>
+  */}
+  <div className="main">
+  <video src= {iroh} className="IrohVideo" autoPlay loop muted />
+      <div class="button">
+      <img class="cupski" src={cupski}></img>
+          
+            POETRY IN MOTION
+      </div>
       </div>
       
    
